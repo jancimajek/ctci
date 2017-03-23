6bc45174dc70eb7bd747fe62a5356f22eca11554
Jan Majek <jan.majek@ft.com>
2017-03-23 12:26:44 +0000

    Refactored binary tree node to improve encapsulation

--- src/DataStructures/BinaryTreeNode.php
+++ src/DataStructures/BinaryTreeNode.php
@@ -26,24 +26,40 @@ class BinaryTreeNode {
     }
 
     public function addLeft($data) {
-        if ($this->left === null) $this->left = new BinaryTreeNode($data);
-        else $this->left->add($data);
+        if ($this->getLeft() === null) $this->setLeft(new BinaryTreeNode($data));
+        else $this->getLeft()->add($data);
     }
 
     public function addRight($data) {
-        if ($this->right === null) $this->right = new BinaryTreeNode($data);
-        else $this->right->add($data);
+        if ($this->getRight() === null) $this->setRight(new BinaryTreeNode($data));
+        else $this->getRight()->add($data);
     }
 
-    public function getLeft() {
-        return $this->left;
+    public function hasLeft() {
+    	return ($this->left !== null);
     }
 
-    public function getRight() {
+	public function getLeft() {
+		return $this->left;
+	}
+
+	public function setLeft(BinaryTreeNode $left = null) {
+		return $this->left = $left;
+	}
+
+	public function hasRight() {
+		return ($this->right !== null);
+	}
+
+	public function getRight() {
         return $this->right;
     }
 
-    public function setData($data) {
+	public function setRight(BinaryTreeNode $right = null) {
+		return $this->right = $right;
+	}
+
+	public function setData($data) {
         $this->data = $data;
     }
 
@@ -85,13 +101,13 @@ class BinaryTreeNode {
     public function toArray() {
         $ret = [];
         $ret['value'] =  $this->getData();
-        $ret['left']  = ($this->left ? $this->left->toArray() : null);
-        $ret['right'] = ($this->right ? $this->right->toArray() : null);
+        $ret['left']  = ($this->hasLeft() ? $this->getLeft()->toArray() : null);
+        $ret['right'] = ($this->hasRight() ? $this->getRight()->toArray() : null);
 
         return $ret;
     }
 
     public function __toString() {
-        return (string)$this->data;
+        return (string)$this->getData();
     }
 }
