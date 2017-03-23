48a3e3034df38b82619c71484528976468dd99f1
Jan Majek <jan.majek@tagman.com>
2017-03-23 01:42:25 +0000

    Refactored BT tests

--- tests/DataStructures/BinaryTreeTest.php
+++ tests/DataStructures/BinaryTreeTest.php
@@ -7,69 +7,76 @@ use PHPUnit\Framework\TestCase;
 
 class BinaryTreeTest extends TestCase
 {
+    private $bt;
+    public function setUp()
+    {
+        $this->bt = new BinaryTree();
+        $this->bt->add(0);
+
+        $this->bt->getRoot()->addLeft(3);
+        $this->bt->getRoot()->addRight(1);
+
+        $this->bt->getRoot()->getLeft()->addLeft(5);
+        $this->bt->getRoot()->getLeft()->addRight(4);
+
+        $this->bt->getRoot()->getRight()->addLeft(8);
+        $this->bt->getRoot()->getRight()->addRight(2);
+
+        $this->bt->getRoot()->getRight()->getLeft()->addRight(9);
+        $this->bt->getRoot()->getRight()->getRight()->addRight(6);
+
+        $this->bt->getRoot()->getRight()->getRight()->getRight()->addRight(7);
+        
+        parent::setUp();
+    }
+
     public function testBinaryTree() {
-        $bt = new BinaryTree();
-        $bt->add(0);
-
-        $bt->getRoot()->addLeft(3);
-        $bt->getRoot()->addRight(1);
-
-        $bt->getRoot()->getLeft()->addLeft(5);
-        $bt->getRoot()->getLeft()->addRight(4);
-
-        $bt->getRoot()->getRight()->addLeft(8);
-        $bt->getRoot()->getRight()->addRight(2);
-
-        $bt->getRoot()->getRight()->getLeft()->addRight(9);
-        $bt->getRoot()->getRight()->getRight()->addRight(6);
-
-        $bt->getRoot()->getRight()->getRight()->getRight()->addRight(7);
-
-        $expectedBtJson = json_encode(json_decode('
-        {
-            "value": 0,
-            "left": {
-                "value": 3,
+        $this->assertEquals((string)$this->bt, json_encode(json_decode('
+            {
+                "value": 0,
                 "left": {
-                    "value": 5,
-                    "left": null,
-                    "right": null
-                },
-                "right": {
-                    "value": 4,
-                    "left": null,
-                    "right": null
-                }
-            },
-            "right": {
-                "value": 1,
-                "left": {
-                    "value": 8,
-                    "left": null,
+                    "value": 3,
+                    "left": {
+                        "value": 5,
+                        "left": null,
+                        "right": null
+                    },
                     "right": {
-                        "value": 9,
+                        "value": 4,
                         "left": null,
                         "right": null
                     }
                 },
                 "right": {
-                    "value": 2,
-                    "left": null,
-                    "right": {
-                        "value": 6,
+                    "value": 1,
+                    "left": {
+                        "value": 8,
                         "left": null,
                         "right": {
-                            "value": 7,
+                            "value": 9,
                             "left": null,
                             "right": null
                         }
+                    },
+                    "right": {
+                        "value": 2,
+                        "left": null,
+                        "right": {
+                            "value": 6,
+                            "left": null,
+                            "right": {
+                                "value": 7,
+                                "left": null,
+                                "right": null
+                            }
+                        }
                     }
                 }
-            }
-        }'), JSON_PRETTY_PRINT);
+            }'), JSON_PRETTY_PRINT)
+        );
+    }
 
-        $this->assertEquals((string)$bt, $expectedBtJson);
-        $this->assertEquals($bt->breadthFirstTraversal(), [0,3,1,5,4,8,2,9,6,7]);
-
+    public function testBreadthFirstTraversal() {
+        $this->assertEquals($this->bt->breadthFirstTraversal(), [0,3,1,5,4,8,2,9,6,7]);
     }
 }
